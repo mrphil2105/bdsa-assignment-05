26802814f21d7b3795d621fff1c1e125b4ee781a
Philip Mørch <mrphil2105@gmail.com>
2022-10-09 15:37:23 +0200

    Make _items and UpdateQuality static

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -2,48 +2,43 @@
 
 public class Program
 {
-    private IList<Item> _items;
+    private static readonly IList<Item> _items = new List<Item>
+    {
+        new() { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+        new() { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+        new() { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+        new() { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+        new() { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
+        new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 },
+        new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49 },
+        new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49 },
+        // this conjured item does not work properly yet
+        new() { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
+    };
 
     private static void Main(string[] args)
     {
         Console.WriteLine("OMGHAI!");
-
-        var app = new Program
-        {
-            _items = new List<Item>
-            {
-                new() { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
-                new() { Name = "Aged Brie", SellIn = 2, Quality = 0 },
-                new() { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
-                new() { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
-                new() { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
-                new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 },
-                new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49 },
-                new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49 },
-                // this conjured item does not work properly yet
-                new() { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
-            }
-        };
 
         for (var i = 0; i < 31; i++)
         {
             Console.WriteLine("-------- day " + i + " --------");
             Console.WriteLine("name, sellIn, quality");
 
-            for (var j = 0; j < app._items.Count; j++)
+            for (var j = 0; j < _items.Count; j++)
             {
-                Console.WriteLine(app._items[j]
-                    .Name + ", " + app._items[j]
-                    .SellIn + ", " + app._items[j]
+                Console.WriteLine(_items[j]
+                    .Name + ", " + _items[j]
+                    .SellIn + ", " + _items[j]
                     .Quality);
             }
 
             Console.WriteLine("");
-            app.UpdateQuality();
+            UpdateQuality();
         }
     }
 
-    public void UpdateQuality()
+    public static void UpdateQuality()
     {
         for (var i = 0; i < _items.Count; i++)
         {
