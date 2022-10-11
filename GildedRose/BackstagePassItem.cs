954e61848274b14c9b287970b373d5e0ed87b7ca
Philip Mørch <mrphil2105@gmail.com>
2022-10-11 20:54:04 +0200

    Create all types of items using Adapter

---
+++ GildedRose/BackstagePassItem.cs
@@ -0,0 +1,34 @@
+﻿namespace GildedRose;
+
+public class BackstagePassItem : Adapter
+{
+    public BackstagePassItem(Item item) : base(item)
+    {
+    }
+
+    public override void Update()
+    {
+        _item.Quality++;
+
+        if (SellIn <= 10)
+        {
+            _item.Quality++;
+        }
+
+        if (SellIn <= 5)
+        {
+            _item.Quality++;
+        }
+
+        _item.SellIn--;
+
+        if (_item.SellIn < 0)
+        {
+            _item.Quality = 0;
+
+            return;
+        }
+
+        base.Update();
+    }
+}
---
+++ GildedRose/NormalItem.cs
@@ -0,0 +1,21 @@
+﻿namespace GildedRose;
+
+public class NormalItem : Adapter
+{
+    public NormalItem(Item item) : base(item)
+    {
+    }
+
+    public override void Update()
+    {
+        _item.SellIn--;
+        _item.Quality--;
+
+        if (SellIn < 0)
+        {
+            _item.Quality--;
+        }
+
+        base.Update();
+    }
+}
