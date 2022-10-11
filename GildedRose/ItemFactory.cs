e98ab5cb1912ec5245618e70d6431412f9dfa15e
Philip Mørch <mrphil2105@gmail.com>
2022-10-11 20:54:35 +0200

    Create item factory

---
+++ GildedRose/ItemFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace GildedRose;
+
+public class ItemFactory : IItemFactory
+{
+    public Adapter Create(Item item)
+    {
+        if (item.Name.Contains("Aged Brie"))
+        {
+            return new AgedBrieItem(item);
+        }
+
+        if (item.Name.Contains("Sulfuras"))
+        {
+            return new LegendaryItem(item);
+        }
+
+        if (item.Name.Contains("Backstage passes"))
+        {
+            return new BackstagePassItem(item);
+        }
+
+        if (item.Name.Contains("Conjured"))
+        {
+            return new ConjuredItem(item);
+        }
+
+        return new NormalItem(item);
+    }
+}
